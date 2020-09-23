4f04cdd82bd9ad81c91b8b4a1f0b52f30e43937e
sven.weinzierl <weinzierl.sven@web.de>
2020-09-23 22:34:36 +0200

    Added method calc relevance scores.

--- xnap/exp/lrp/lrp.py
+++ xnap/exp/lrp/lrp.py
@@ -6,12 +6,12 @@ import numpy as np
 
 
 def calc_and_plot_relevance_scores(trace, args, preprocessor):
-
     heatmap: str = ""
 
     for idx in range(2, len(trace)):
         # next activity prediction
-        predicted_act_class, target_act_class, target_act_class_str, prefix_words, model, input_encoded, prob_dist = test.test_prefix(args, preprocessor, trace, idx)
+        predicted_act_class, target_act_class, target_act_class_str, prefix_words, model, input_encoded, prob_dist = test.test_prefix(
+            args, preprocessor, trace, idx)
         print("Prefix: %s; Next activity prediction: %s; Next activity target: %s" % (
             idx, predicted_act_class, target_act_class_str))
         print("Probability Distribution:")
@@ -24,7 +24,30 @@ def calc_and_plot_relevance_scores(trace, args, preprocessor):
 
         Rx, Rx_rev, R_rest = net.lrp(prefix_words, target_act_class, eps, bias_factor)  # perform LRP
         R_words = np.sum(Rx + Rx_rev, axis=1)  # compute word-level LRP relevances
-        scores = net.s.copy()  # classification prediction scores
+        # scores = net.s.copy()  # classification prediction scores
 
         heatmap = heatmap + html_heatmap(prefix_words, R_words) + "<br>"  # create heatmap
         browser.display_html(heatmap)  # display heatmap
+
+
+def calc_relevance_scores(trace, args, preprocessor):
+
+    scores_prefix = list()
+
+    for idx in range(2, len(trace)):
+        # next activity prediction
+        predicted_act_class, target_act_class, target_act_class_str, prefix_words, model, input_encoded, prob_dist = test.test_prefix(
+            args, preprocessor, trace, idx)
+
+        # compute lrp relevances
+        eps = 0.001  # small positive number
+        bias_factor = 0.0  # recommended value
+        net = LSTM_bidi(args, model, input_encoded)  # load trained LSTM model
+
+        Rx, Rx_rev, R_rest = net.lrp(prefix_words, target_act_class, eps, bias_factor)
+
+        # todo: relevance scores for activity and attributes
+        scores = np.sum(Rx + Rx_rev, axis=1)  # compute word-level LRP relevances
+        scores_prefix.append(scores)
+
+    return scores_prefix
--- xnap/runner.py
+++ xnap/runner.py
@@ -41,9 +41,11 @@ if __name__ == '__main__':
 
     elif args.mode == 1:
         preprocessor = Preprocessor(args)
-
         trace = preprocessor.get_random_process_instance(args.rand_lower_bound, args.rand_upper_bound)
         lrp.calc_and_plot_relevance_scores(trace, args, preprocessor)
 
     else:
-        print("Not yet implemented ...")
+        preprocessor = Preprocessor(args)
+        trace = preprocessor.get_random_process_instance(args.rand_lower_bound, args.rand_upper_bound)
+        scores = lrp.calc_relevance_scores(trace, args, preprocessor)
+        print(scores)
