db95a21071a076dc989b63e6ee05116876def1a7
Lena Cabrera <lena.m.cabrera@gmail.com>
2020-11-16 02:31:31 +0100

    Added first prototype for explainability with LIME (LimeTextExplainer).

--- xnap/exp/lime/lime.py
+++ xnap/exp/lime/lime.py
@@ -2,6 +2,9 @@ import xnap.nap.tester as test
 from lime.lime_text import LimeTextExplainer
 from tensorflow.keras.models import load_model
 import numpy
+from xnap.exp.lrp.util.heatmap import html_heatmap
+import xnap.exp.lrp.util.browser as browser
+import pandas as pd
 import re
 
 # TODO add lime to requirements
@@ -12,10 +15,8 @@ global args_
 global preprocessor_
 global event_log_
 
-# string delimiters
-DELIMITER_ATTR = "ATTRIBUTE"
-DELIMITER_EVENT = "EVENT"
-DELIMITER_ADD = " "
+DELIMITER = " "
+prefix_lookup = {}
 
 def calc_and_plot_relevance_scores_instance(event_log, case, args, preprocessor):
 
@@ -26,6 +27,8 @@ def calc_and_plot_relevance_scores_instance(event_log, case, args, preprocessor)
     args_ = args
     preprocessor_ = preprocessor
     event_log_ = event_log
+
+    heatmap: str = ""
 
     for prefix_size in range(2, len(case)):
         # # next activity prediction
@@ -38,62 +41,79 @@ def calc_and_plot_relevance_scores_instance(event_log, case, args, preprocessor)
 
         # LIME
         subseq_case = case[:prefix_size]
-        subseq_case_str = transform_subseq_to_string(subseq_case, args, preprocessor)
-        class_names = list(preprocessor.activity['labels_to_chars'].keys())
+        set_prefix_lookup(subseq_case)
+        event_ids_str = DELIMITER.join([str(id) for id in prefix_lookup.keys()])
 
-        # TODO maybe use split_expression to correct perturbation (Docu "split_expression – strings will be split by this.")
-        # https://github.com/marcotcr/lime/issues/27
-        # bear in mind: dot in decimal numbers, DELIMITERs
-        # split_expression = '|'.join(['\W+', 'word1', 'word2'])
-        # split_expression = re.split(r'\W+', s)
+        explainer = LimeTextExplainer() # TODO check if class_names are necessary
+        wrapped_classifier_function = wrapped_classifier_fn([event_ids_str])  # function returns function!
+        explanations = explainer.explain_instance(event_ids_str,
+                                                  wrapped_classifier_function,
+                                                  num_samples=5  # number of perturbed strings per prefix, default=5000
+                                                  )
 
-        explainer = LimeTextExplainer(class_names=class_names) #, split_expression=split_expression)
-        # wrapped_classifier_fn = get_predict_proba_fn_of_class([subseq_case_str]) # uncomment for better debugging of nested functions (if they are not nested/unindented)
-        wrapped_classifier_function = wrapped_classifier_fn([subseq_case_str]) # function returns function!
-        explanations = explainer.explain_instance(subseq_case_str, wrapped_classifier_function)
+        prefix_words, R_words, R_words_context = create_heatmap_data(args, preprocessor, event_log, subseq_case,
+                                                                     explanations)
 
-def transform_subseq_to_string(subseq_case, args, preprocessor):
-    """ Transforms a subsequence of events (list) to a string. """
+        # heatmap
+        heatmap = heatmap + html_heatmap(prefix_words, R_words, R_words_context) + "<br>"  # create heatmap
+        browser.display_html(heatmap)  # display heatmap
+
+
+def transform_subseq_to_lists(subseq_case, args, preprocessor):
+    """ Transforms a subsequence of events (list) to a list. """
     subseq_str_list = []
     for event in subseq_case:
-        activity_char = get_activity_char(event, args, preprocessor)
-        context = []
+        event_list = []
+        activity_enc = tuple(event[args.activity_key])
+        activity_id = preprocessor.activity['one_hot_to_event_ids'][activity_enc]
+        activity = preprocessor.unique_event_ids_map_to_name[activity_id]
+        event_list.append(activity)
         for context_name in preprocessor.context['attributes']:
-            attr_str = get_context_attribute_str_value(event, context_name, preprocessor)
+            context_enc = event[context_name]
+            context_val = context_enc
+            if isinstance(context_enc, list):
+                context_val = preprocessor.context['enc_to_val'][context_name][tuple(context_enc)]
+            event_list.append(context_val)
+        subseq_str_list.append(event_list)
+    return subseq_str_list
 
-            # TODO remove (for testing purposes convert float to int as decimal 'dot' in float causes wrong perturbation)
-            attr_idx = preprocessor.context['attributes'].index(context_name)
-            attr_dtype = preprocessor.context['data_types'][attr_idx]
-            if attr_dtype == 'num':
-                # event_enc.append(float(attr_val))
-                attr_str = str(int(float(attr_str)))
+def set_prefix_lookup(subseq_case):
+    """ Maps events of a prefix to unique IDs. """
+    global prefix_lookup
+    for idx, event in enumerate(subseq_case):
+        prefix_lookup[idx] = event
 
-            context.append(attr_str)
-        context_str = DELIMITER_ADD.join(context)
-        # context_str = (DELIMITER_ADD + DELIMITER_ATTR + DELIMITER_ADD).join(context)
-        event_str = activity_char + DELIMITER_ADD + context_str
-        # event_str = activity_char + DELIMITER_ADD + DELIMITER_ATTR + DELIMITER_ADD + context_str
-        subseq_str_list.append(event_str)
-    subseq_str = " ".join(subseq_str_list)
-    # subseq_str = (DELIMITER_ADD + DELIMITER_EVENT + DELIMITER_ADD).join(subseq_str_list)
-    return subseq_str
+def create_heatmap_data(args, preprocessor, event_log, subseq_case, explanations):
+    """ Prepares explanation data for heatmap visualization """
+    exp_dict = dict(explanations.as_list())
 
-def get_activity_char(event, args, preprocessor):
-    """ Returns a char representing the activity of an event. """
-    activity_encoding = tuple(event[args.activity_key])
-    activity_char = preprocessor.activity['labels_to_chars'][activity_encoding]
-    return activity_char
+    mapping_id_act = {}
+    for event_id, event in prefix_lookup.items():
+        mapping_id_act[event_id] = tuple(event[args_.activity_key])
 
-def get_context_attribute_str_value(event, attr_name, preprocessor):
-    """ Returns original string value of a context attribute for an event. """
-    attr_enc = event[attr_name]
-    if isinstance(attr_enc, list):
-        # categorical value -> retrieve original value
-        attr_str = str(preprocessor.context['enc_to_val'][attr_name][tuple(attr_enc)])
-    else:
-        # numerical value -> keep encoded value
-        attr_str = str(attr_enc)
-    return attr_str
+    activities = []
+    for event_id_str in exp_dict.keys():
+        activities.append(mapping_id_act[int(event_id_str)])
+    relevance_scores = list(exp_dict.values())
+    relevance_dict = dict(zip(activities, relevance_scores))
+
+    max_case_len = preprocessor.get_max_case_length(event_log)
+
+    prefix_words = transform_subseq_to_lists(subseq_case, args, preprocessor)
+
+    R_words = numpy.zeros(max_case_len)
+    idx = max_case_len - 1
+    for event in subseq_case:
+        activity = event[args.activity_key]
+        relevance_score = relevance_dict[tuple(activity)]
+        R_words[idx] = relevance_score
+        idx -= 1
+
+    R_words_context = {}
+    for context_attr in preprocessor.context['attributes']:
+        R_words_context[context_attr] = numpy.zeros(max_case_len)
+
+    return prefix_words, R_words, R_words_context
 
 def wrapped_classifier_fn(subseq_str_list):
     """ Performs preprocessing and prediction for use of LIME. """
@@ -105,12 +125,13 @@ def wrapped_classifier_fn(subseq_str_list):
             args_.model_dir,
             args_.task,
             args_.data_set[0:len(args_.data_set) - 4], model_index))
+        pred_probab = []
         for subseq_str in subseq_str_list:
-            # TODO create tensor from all perturbed strings?
             data_tensor = transform_string_to_tensor(subseq_str)
             y = model.predict(data_tensor)
             y = y[0][:]
-            return y
+            pred_probab.append(y)
+        return numpy.asarray(pred_probab)
 
     def transform_string_to_tensor(subseq_str):
         """ Produces a vector-oriented representation of feature data as a 3-dimensional tensor from a string. """
@@ -121,79 +142,37 @@ def wrapped_classifier_fn(subseq_str_list):
                                        max_case_length,
                                        num_features), dtype=numpy.float32)
 
-        subseq = get_event_lists_from_string(subseq_str)
-        subseq_enc = encode_subseq(subseq, preprocessor_)
+        left_pad = max_case_length - len(subseq_str)
+        timestep = 0
+        for event_id_str in subseq_str:
+            if " " in event_id_str:
+                continue
+            event_id = int(event_id_str)
+            event = prefix_lookup[event_id]
 
-        left_pad = max_case_length - len(subseq_enc)
-        for timestep, event in enumerate(subseq_enc):
-            for attr_idx, attr_enc in enumerate(event):
-                if attr_idx == 0:
-                    # activity
-                    for idx, val in enumerate(attr_enc):
-                        features_tensor[0, timestep + left_pad, idx] = val
-                else:
-                    # context
-                    start_idx = 0
-                    if not isinstance(attr_enc, list):
-                        features_tensor[
-                            0, timestep + left_pad, start_idx + preprocessor_.get_length_of_activity_label()] = attr_enc
+            # activity
+            activity_enc = event[args_.activity_key]
+            for idx, val in enumerate(activity_enc):
+                features_tensor[0, timestep + left_pad, idx] = val
+
+            # context
+            if preprocessor_.context_exists():
+                start_idx = 0
+                for attribute_idx, attribute_key in enumerate(preprocessor_.context['attributes']):
+                    attribute_values = event.get(attribute_key)
+
+                    if not isinstance(attribute_values, list):
+                        features_tensor[0, timestep + left_pad,
+                                        start_idx + preprocessor_.get_length_of_activity_label()] = attribute_values
                         start_idx += 1
                     else:
-                        for idx, val in enumerate(attr_enc, start=start_idx):
-                            features_tensor[
-                                0, timestep + left_pad, idx + preprocessor_.get_length_of_activity_label()] = val
-                        start_idx += len(attr_enc)
+                        for idx, val in enumerate(attribute_values, start=start_idx):
+                            features_tensor[0, timestep + left_pad,
+                                            idx + preprocessor_.get_length_of_activity_label()] = val
+                        start_idx += len(attribute_values)
+
+            timestep += 1
 
         return features_tensor
-
-    def get_event_lists_from_string(str):
-        """ Converts a string into a list of strings (words)"""
-        # str = str.replace(DELIMITER_ATTR, DELIMITER_ADD)
-        str_list = [value for value in list(str.split(DELIMITER_ADD)) if value != DELIMITER_ATTR]
-        subseq = []
-        event = []
-        for attr_idx, attr_val in enumerate(str_list):
-            # if attr_val != DELIMITER_EVENT:
-            if attr_idx == 0:
-                # add first activity
-                event.append(attr_val)
-            else:
-                if len(attr_val) == 1 and not attr_val.isnumeric():
-                    # if attribute is activity (char)
-                    subseq.append(event)
-                    event = [attr_val]
-                else:
-                    # if attribute is context
-                    event.append(attr_val)
-        subseq.append(event)
-        return subseq
-
-    def encode_subseq(subseq, preprocessor):
-        """ Restores encoding of all event attribute values. """
-        subseq_enc = []
-        event_enc = []
-        for event in subseq:
-            for attr_idx, attr_val in enumerate(event):
-                context_idx = 0
-                if attr_idx == 0:
-                    activity_enc = preprocessor.activity['chars_to_labels'][attr_val]
-                    event_enc.append(list(activity_enc))
-                else:
-                    # context
-                    attr_dtype = preprocessor.context['data_types'][attr_idx - 1]
-                    if attr_dtype == 'num':
-                        # event_enc.append(float(attr_val))
-                        event_enc.append(int(float(attr_val)))
-                    else:
-                        attr_name = preprocessor.context['attributes'][attr_idx - 1]
-                        if attr_val.strip('-').isnumeric():
-                            attr_val = int(attr_val)
-                        attr_enc = preprocessor.context['val_to_enc'][attr_name][attr_val]
-                        if isinstance(attr_enc, tuple):
-                            attr_enc = list(attr_enc)
-                        event_enc.append(attr_enc)
-            subseq_enc.append(event_enc)
-            event_enc = []
-        return subseq_enc
 
     return classifier_fn
