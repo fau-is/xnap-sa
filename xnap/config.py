f02ae5afae8562da02712277afbbff8b897c4c8c
sven.weinzierl <weinzierl.sven@web.de>
2021-01-12 20:11:35 +0100

    Update of files.

--- xnap/config.py
+++ xnap/config.py
@@ -7,16 +7,16 @@ def load():
     parser = argparse.ArgumentParser()
 
     # General parameters
-    parser.add_argument('--mode', default=2, type=int)
+    parser.add_argument('--mode', default=0, type=int)
     """ There are three modes
         0 = train and test model
         1 = explain prediction for random process instance
         2 = evaluate explanations for predictions (test set) 
     """
     # Mode 1 + 2
-    parser.add_argument('--xai', default="lrp", type=str)  # lrp, lime, shap
-    parser.add_argument('--lime_num_samples', default=50, type=int)  # 5000 (default)
-    parser.add_argument('--shap_num_samples', default=100, type=int)  # 100 (good estimate), 1000 (very good estimate)
+    parser.add_argument('--xai', default="lime", type=str)  # lrp, lime, shap
+    parser.add_argument('--lime_num_samples', default=10, type=int)  # 5000 (default)
+    parser.add_argument('--shap_num_samples', default=10, type=int)  # 100 (good estimate), 1000 (very good estimate)
     # Mode 1
     parser.add_argument('--rand_lower_bound', default=6, type=int)
     parser.add_argument('--rand_upper_bound', default=6, type=int)
@@ -31,14 +31,14 @@ def load():
     parser.add_argument('--classifier', default="LSTM", type=str)  # LSTM, RF, DT
 
     # Parameters for deep neural network
-    parser.add_argument('--dnn_num_epochs', default=3, type=int)
+    parser.add_argument('--dnn_num_epochs', default=100, type=int)
     parser.add_argument('--dnn_architecture', default=0, type=int)
     parser.add_argument('--learning_rate', default=0.002, type=float)
     parser.add_argument('--dim', default=0, type=int)
 
     # Directories
     parser.add_argument('--task', default="nap")
-    parser.add_argument('--data_set', default="helpdesk_raw_sample_100.csv")
+    parser.add_argument('--data_set', default="helpdesk_raw.csv")
     parser.add_argument('--data_dir', default="./data/")
     parser.add_argument('--model_dir', default="nap/models/")
     parser.add_argument('--result_dir', default="./results/")
@@ -47,7 +47,7 @@ def load():
     parser.add_argument('--seed', default=True, type=utils.str2bool)
     parser.add_argument('--seed_val', default=1377, type=int)
     parser.add_argument('--shuffle', default=True, type=int)
-    parser.add_argument('--split_rate_train', default=0.8, type=float)
+    parser.add_argument('--split_rate_train', default=0.99, type=float)
     parser.add_argument('--val_split', default=0.1, type=float)
     parser.add_argument('--batch_size_train', default=128, type=int)
     parser.add_argument('--batch_size_test', default=1, type=int)
